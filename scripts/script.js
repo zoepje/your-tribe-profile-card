6b06d7417d58335ea34f71f929833f9a98331f95
zoepje <zoe.penner@hva.nl>
2023-09-07 12:54:50 +0200

    edit js css html
    een button er bij zodat we uiteindelijk het kaartje kunnen flippen
    en kleuren en lettertypes verandert

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const button = document.querySelector("#flip1");
+const button1 = document.querySelector("#flip2");
 
-function expand () {
-  document.body.classList.toggle('expand')
-}
+var back = document.querySelector("#beneden");
+var front = document.querySelector("#boven");
+
+button.addEventListener("click", flip);
+button1.addEventListener("click", flip);
+
+function flip(){
+  document.body.classList.toggle("flip");
+  }
+
